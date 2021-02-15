76ad75e0c2c71c8213e60117f56a5ba9c5bec066
Tom Paynter <tompaynter@tdpe.co.uk>
2021-02-15 17:43:16 +0000

    Adding more material properties and skinnable materials.

--- src/tiny_gltf.cpp
+++ src/tiny_gltf.cpp
@@ -2,12 +2,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_IMPLEMENTATION
 #define TINYGLTF_IMPLEMENTATION
+#define TINYGLTF_USE_CPP14
 
 // If using a modern Microsoft Compiler, this define supress compilation
 // warnings in stb_image_write
 #ifdef TP_WIN32
 #define STBI_MSC_SECURE_CRT
 #endif
-
 
 #include "tiny_gltf.h"
